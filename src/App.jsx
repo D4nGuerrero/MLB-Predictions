3dfc088651ad94fdd0a7a05da48866809201ac8f
Adrian1jr <adrian1jr@gmail.com>
2024-02-26 23:09:36 -0600

    Added design for league and updated design for overall

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import NextUITabs from "./components/Tabs";
 import teams from "./teams.json";
 
 function App() {
+  const mappedTeams = teams.map((team) => ({
+    ...team,
+    losses: 0,
+    pct: 0,
+    gb: 0,
+  }));
+
   const tabsData = {
     division: {
       columns: [
@@ -11,7 +18,7 @@ function App() {
         { name: "PCT", uid: "pct" },
         { name: "GB", uid: "gb" },
       ],
-      rows: teams,
+      rows: mappedTeams,
     },
 
     league: {
@@ -22,7 +29,7 @@ function App() {
         { name: "PCT", uid: "pct" },
         { name: "GB", uid: "gb" },
       ],
-      rows: teams,
+      rows: mappedTeams,
     },
 
     overall: {
@@ -33,13 +40,13 @@ function App() {
         { name: "PCT", uid: "pct" },
         { name: "GB", uid: "gb" },
       ],
-      rows: teams,
+      rows: mappedTeams,
     },
   };
 
   return (
-    <div className="bg-gray-900 flex flex-col items-center h-screen py-8 px-12">
-      <h2 className="text-4xl text-slate-200 font-bold sm:text-5xl mb-6">
+    <div className="bg-gray-900 items-center py-8 px-12">
+      <h2 className="text-4xl text-slate-200 font-bold sm:text-5xl mb-6 text-center">
         MLB Standings
       </h2>
       <NextUITabs data={tabsData} />
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -5,6 +5,14 @@ import NextUITable from "./Table";
 export default function NextUITabs({ data }) {
   const [selected, setSelected] = useState("overall");
 
+  const NationalLeagueTeams = data.league.rows.filter(
+    (team) => team.league === "NL"
+  );
+
+  const AmericanLeagueTeams = data.league.rows.filter(
+    (team) => team.league === "AL"
+  );
+
   return (
     <Tabs
       aria-label="Options"
@@ -12,6 +20,7 @@ export default function NextUITabs({ data }) {
       onSelectionChange={setSelected}
       radius="lg"
       color="primary"
+      className="flex justify-center"
       classNames={{
         tabContent: "text-medium text-white",
         tabList: "bg-gray-800",
@@ -25,23 +34,41 @@ export default function NextUITabs({ data }) {
           tabKey={"division"}
         />
       </Tab>
-      <Tab key="league" title="League">
-        <h1 className="text-white">
+      <Tab
+        key="league"
+        title="League"
+        className="flex flex-wrap w-full sm:flex-wrap md:flex-wrap lg:flex-wrap xl:flex-nowrap"
+      >
+        <div className="w-full">
+          <h1 className="text-slate-200 font-bold text-3xl text-center">
+            American League
+          </h1>
+
           <NextUITable
             columns={data.league.columns}
-            rows={data.league.rows}
+            rows={AmericanLeagueTeams}
             tabKey={"league"}
           />
-        </h1>
+        </div>
+
+        <div className="w-full">
+          <h1 className="text-slate-200 font-bold text-3xl text-center">
+            National League
+          </h1>
+
+          <NextUITable
+            columns={data.league.columns}
+            rows={NationalLeagueTeams}
+            tabKey={"league"}
+          />
+        </div>
       </Tab>
       <Tab key="overall" title="Overall">
-        <h1 className="text-white">
-          <NextUITable
-            columns={data.overall.columns}
-            rows={data.overall.rows}
-            tabKey={"overall"}
-          />
-        </h1>
+        <NextUITable
+          columns={data.overall.columns}
+          rows={data.overall.rows}
+          tabKey={"overall"}
+        />
       </Tab>
     </Tabs>
   );
